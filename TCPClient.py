bcb8e016780a2aee18f509544bc2f8820e6698d4
Leonardo Ruma <leonardo.ruma@gmail.com>
2023-05-08 22:35:14 -0300

    melhorando a conexão tcp. Agora, como a conexão é persistente eu consigo fazer com que o servidor diga que a conexão foi feita com sucesso e responda com a lista das palavras disponiveis para tradução.

--- TCPClient.py
+++ TCPClient.py
@@ -1,38 +1,35 @@
 import socket
 
 def main():
-    # Cria um socket TCP/IP
+    # Configurações do cliente
+    HOST = 'localhost'
+    PORT = 12003
+
+    # Criação do socket TCP/IP
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Endereço IP e Porta do servidor
-    server_address = ('localhost', 12001)
+    # Conexão com o servidor
+    client_socket.connect((HOST, PORT))
 
-    # Conecta ao servidor
-    client_socket.connect(server_address)
+    # Recebe a lista de palavras disponíveis
+    lista_palavras = client_socket.recv(1024).decode('utf-8')
+    print(f"Palavras disponíveis: {lista_palavras}")
 
-    # Recebe a lista de palavras disponíveis do servidor
-    data = client_socket.recv(1024).decode()
-    print('Palavras disponíveis:', data)
-
-    # Loop principal para a tradução de palavras
     while True:
-        # Lê a palavra do usuário
-        palavra = input('Digite uma palavra para traduzir ou "sair" para encerrar: ').lower()
+        # Solicita a palavra a ser traduzida
+        palavra = input("Digite a palavra a ser traduzida (ou 'sair' para encerrar conexão): ")
 
-        # Encerra o loop se o usuário digitar "sair"
         if palavra == 'sair':
             break
 
         # Envia a palavra ao servidor
-        client_socket.sendall(palavra.encode())
+        client_socket.sendall(palavra.encode('utf-8'))
 
-        # Recebe a resposta do servidor
-        data = client_socket.recv(1024).decode()
+        # Recebe a tradução do servidor
+        data = client_socket.recv(1024).decode('utf-8')
+        print(f"Tradução: {data}")
 
-        # Exibe a resposta
-        print('Tradução:', data)
-
-    # Fecha o socket
+    # Encerra a conexão com o servidor
     client_socket.close()
 
 if __name__ == '__main__':
--- TCPServer.py
+++ TCPServer.py
@@ -2,39 +2,61 @@ import socket
 
 
 def traduzir(palavra):
-    dicionario = {
-        'casa': 'house',
-        'carro': 'car',
-        'escola': 'school',
-        'trabalho': 'work',
-        'telefone': 'phone',
-        'computador': 'computer',
-        'música': 'music',
-        'livro': 'book',
-        'tempo': 'time'
-    }
-    return dicionario.get(palavra, 'Palavra não encontrada no dicionário.')
+    if palavra == 'casa':
+        return 'house'
+    elif palavra == 'carro':
+        return 'car'
+    elif palavra == 'escola':
+        return 'school'
+    elif palavra == 'trabalho':
+        return 'work'
+    elif palavra == 'telefone':
+        return 'phone'
+    elif palavra == 'computador':
+        return 'computer'
+    elif palavra == 'música':
+        return 'music'
+    elif palavra == 'livro':
+        return 'book'
+    elif palavra == 'tempo':
+        return 'time'
+    else:
+        return 'Palavra não encontrada no dicionário.'
 
 
 def main():
     # Configurações do servidor
     HOST = 'localhost'
-    PORT = 12001
+    PORT = 12003
+
+    # Lista de palavras disponíveis
+    palavras_disponiveis = [
+        'casa', 'carro', 'escola', 'trabalho', 'telefone',
+        'computador', 'música', 'livro', 'tempo'
+    ]
 
     # Criação do socket TCP/IP
-    servidor_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    servidor_socket.bind((HOST, PORT))
-    servidor_socket.listen(1)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Vincula o socket à uma porta
+    server_socket.bind((HOST, PORT))
+
+    # Define o limite de conexões pendentes
+    server_socket.listen(1)
     print(f"Servidor TCP escutando na porta {PORT}")
 
     while True:
         # Esperando por uma conexão
-        cliente_socket, cliente_endereco = servidor_socket.accept()
-        print(f"Conexão estabelecida com {cliente_endereco}")
+        client_socket, client_address = server_socket.accept()
+        print(f"Conexão estabelecida com {client_address}")
+
+        # Envia a lista de palavras disponíveis ao cliente
+        palavras_disponiveis_str = ', '.join(palavras_disponiveis)
+        client_socket.sendall(palavras_disponiveis_str.encode('utf-8'))
 
         while True:
             # Recebe a mensagem do cliente
-            data = cliente_socket.recv(1024).decode('utf-8')
+            data = client_socket.recv(1024).decode('utf-8')
 
             if not data:
                 break
@@ -46,11 +68,12 @@ def main():
             print(f"Tradução: {resultado}")
 
             # Envia a resposta ao cliente
-            cliente_socket.sendall(resultado.encode('utf-8'))
+            client_socket.sendall(resultado.encode('utf-8'))
 
         # Fecha o socket do cliente
-        cliente_socket.close()
-        print(f"Conexão com {cliente_endereco} finalizada")
+        client_socket.close()
+        print(f"Conexão com {client_address} finalizada")
+
 
 
 if __name__ == '__main__':
